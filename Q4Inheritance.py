c9785dd1fd6cc441058742ebcaf62c30fc1fb0d3
samshadv <mohdsamshad1436@gmail.com>
2026-04-11 13:36:42 +0530

    Add class implementation for Animal and Dog with speak methods

---
+++ Q4Inheritance.py
@@ -0,0 +1,20 @@
+class Animal:
+    
+    def __init__(self,name):
+        self.name = name
+    
+    def speak(self):
+        print("Animal make sound ")
+        
+class Dog(Animal):
+    
+    def __init__(self,name,sound):
+        super().__init__(name)
+        self.sound = sound
+        
+    def speak(self):
+        print(f"{self.name} barks: {self.sound}")
+        
+
+d = Dog("Tommy", "Woof")   
+d.speak()
