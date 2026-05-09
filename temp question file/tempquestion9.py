8d02eeaa4a2656da9499fa6451f457c3a24ad058
samshadv <mohdsamshad1436@gmail.com>
2026-05-09 11:38:36 +0530

    Refactor BankAccount class to use private balance attribute for encapsulation

--- temp question file/tempquestion9.py
+++ temp question file/tempquestion9.py
@@ -1,19 +1,19 @@
 class BankAccount:
     def __init__(self,acc_no,balance):
         self.acc_no = acc_no
-        self.balance = balance
+        self.__balance = balance
         
     def deposit(self,amount):
-        self.balance += amount
+        self.__balance += amount
         
     def withdraw(self,amount):
-        if amount>self.balance:
+        if amount>self.__balance:
             print("not money")
         else:
-            self.balance -= amount
+            self.__balance -= amount
             
     def check_balance(self):
-        print(self.balance)
+        print(self.__balance)
         
 acc = BankAccount(101,1000)
 
